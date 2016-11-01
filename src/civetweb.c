09c580e9aa11a4e75ceaebde8ae18b6e0d43bf9e
Yehuda Sadeh <yehuda@redhat.com>
2016-11-01 16:22:39 -0700

    civetweb: return 400 instead of 411 on invalid input
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/civetweb.c
+++ src/civetweb.c
@@ -11925,7 +11925,7 @@ getreq(struct mg_connection *conn, char *ebuf, size_t ebuf_len, int *err)
 				            ebuf_len,
 				            "%s",
 				            "Bad Request");
-				*err = 411;
+				*err = 400;
 				return 0;
 			}
 			/* Publish the content length back to the request info. */
